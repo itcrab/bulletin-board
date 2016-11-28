1433e22de430bb5cd48502e30ad201b1e129a98c
Arcady Usov <itcrab@gmail.com>
2016-11-28 15:16:14 +0500

    Add index and detail views for board

--- board/models.py
+++ board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Bulletin(models.Model):
+    title = models.CharField('заголовок', max_length=255)
+    description = models.TextField('описание')
+    pub_date = models.DateTimeField('опубликовано', auto_now_add=True, null=True)
+    upd_date = models.DateTimeField('обновлено', auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
--- board/views.py
+++ board/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Bulletin
+
+
+def index(request):
+    bulletin_list = Bulletin.objects.order_by('-pub_date').all()
+    bulletins = _paginate_records(bulletin_list, 5, request)
+
+    return render(request, 'board/index.html', {
+        'bulletins': bulletins,
+    })
+
+
+def detail(request, bulletin_id):
+    bulletin = get_object_or_404(Bulletin, pk=bulletin_id)
+
+    return render(request, 'board/detail.html', {
+        'bulletin': bulletin,
+    })
+
+
+def _paginate_records(records_list, count_per_page, request):
+    paginator = Paginator(records_list, count_per_page)
+
+    page = request.GET.get('page')
+    try:
+        records = paginator.page(page)
+    except PageNotAnInteger:
+        records = paginator.page(1)
+    except EmptyPage:
+        records = paginator.page(paginator.num_pages)
+
+    return records
